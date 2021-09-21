cd204f56781a7afcdcb339421295fcedf844bafb
libra <23025996+smirror@users.noreply.github.com>
2021-09-21 17:08:58 +0900

    Add ==, !=, <= and >= operators

--- test.sh
+++ test.sh
@@ -16,17 +16,17 @@ assert() {
   fi
 }
 
-# Step01
+# step01
 assert 0 0
 assert 42 42
 
-# Step02
+# step02
 assert 21 "5+20-4"
 
-# Step03
+# step03
 assert 41 " 12 + 34 - 5 "
 
-# Step05
+# step05
 assert 47 '5+6*7'
 assert 15 '5*(9-6)'
 assert 4 '(3+5)/2'
@@ -35,5 +35,25 @@ assert 4 '(3+5)/2'
 assert 10 '-10+20'
 assert 10 '- -10'
 assert 10 '- - +10'
+
+# step07
+assert 0 '0==1'
+assert 1 '42==42'
+assert 1 '0!=1'
+assert 0 '42!=42'
+
+assert 1 '0<1'
+assert 0 '1<1'
+assert 0 '2<1'
+assert 1 '0<=1'
+assert 1 '1<=1'
+assert 0 '2<=1'
+
+assert 1 '1>0'
+assert 0 '1>1'
+assert 0 '1>2'
+assert 1 '1>=0'
+assert 1 '1>=1'
+assert 0 '1>=2'
 
 echo OK
